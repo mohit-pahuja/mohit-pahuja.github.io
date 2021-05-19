dd056087017c6491c59d1d06cbe4d6e079fd6edf
Mohit Pahuja <mohit.pahuja@paytmbank.com>
2021-05-20 00:06:32 +0530

    url update

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://mohitp1896.github.io'
+SITEURL = 'http://mohit-pahuja.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
